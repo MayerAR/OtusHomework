0f2cb7155d1e280d2e8554ba4824792653503848
Artur <43312411+MayerAR@users.noreply.github.com>
2023-10-11 14:41:44 +0300

    New branch

--- main.cpp
+++ main.cpp
@@ -1,27 +1,7 @@
 #include <iostream>
-#include <vector>
-#include <list>
-#include <map>
-#include <ranges>
-#include <algorithm>
-#include <numeric>
-#include "allocator.h"
-
-using allocator = my_allocator::heap::allocator<std::pair<const int, int>>;
 
 int main (int, char **) 
 {
-    allocator alloc(10);
-    std::map<int, int, std::less<int>, allocator> m1(alloc);
-    m1[0] = 1;
-    for (int i = 1; i < 9; ++i)
-    {
-        m1[i] = i * m1[i - 1];
-    }
-
-    for (auto& [key, value] : m1)
-        std::cout << key << ' ' << value << '\n';
-
     std::cin.get();
     return 0;
 }
