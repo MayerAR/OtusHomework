fe5b348ee826b5c800c9a4132fef616fd49e5a86
Artur <43312411+MayerAR@users.noreply.github.com>
2023-11-06 13:56:55 +0300

    test

--- main.cpp
+++ main.cpp
@@ -1,3 +1,21 @@
+
+/*! \mainpage Name or Title or the Software or the Project.
+ *
+ * \section intro_sec Introduction(or any thing)
+ *
+ * bla bla bla
+ * more and more bla
+ *
+ * \section install_sec How to Install(or any thing)
+ *
+  * \subsection creating_sub_sec Compiling(or any thing)
+  *
+  * bla bla and bla
+  * bla and bla bla
+  *
+  *
+  */
+
 #include "lib.h"
 
 #include <iostream>
